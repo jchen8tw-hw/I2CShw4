20f00a97464b88ca421b43d20ae5c3f6b08754b9
tzyLee <39000844+tzyLee@users.noreply.github.com>
2019-04-29 23:08:23 +0800

    Fix bug in msp.pl

--- msp.pl
+++ msp.pl
@@ -1,7 +1,27 @@
-max_sum([],0,0).
-max_sum([A|B],Cursum,Maxsum):-
-    max_sum( B,Prev_cursum,Prev_maxsum),
-    ( (Prev_cursum + A )> 0 -> Cursum is(Prev_cursum+A) ; (Prev_cursum + A ) =< 0 -> Cursum is 0),
-    ( (Cursum) > Prev_maxsum -> Maxsum is Cursum ; (Cursum) =< Prev_maxsum ->  Maxsum is Prev_maxsum).
-maxsum(L,M):-
-    max_sum(L,K,M).    
+/* Use 0 if Result is negative */
+maxSum(X, Sum):-
+    maxSumImpl(X, _, Sum).
+/* Reversed kadane's algorithm (Tail recursive) */
+/* Base case
+ maxSumStartWithHead([], 0).
+   Maximum subarray sum starts with X = X or X + maxSumStartWithHead(Xs)
+ maxSumStartWithHead([X|Xs], MaxStartWithX):-
+     maxSumStartWithHead(Xs, MaxAfterX),
+	 MaxStartWithX is max(X, X+MaxAfterX).
+*/
+
+maxSumImpl([], 0, 0).
+maxSumImpl([X|Xs], MaxStartWithX, OverallMax):-
+    maxSumImpl(Xs, MaxAfterX, OverallMaxWithoutX),
+	MaxStartWithX is max(X, X+MaxAfterX),
+	OverallMax is max(OverallMaxWithoutX, MaxStartWithX).
+	
+/* Tests
+
+maxSum([], A).
+maxSum([1], A).
+maxSum([-1], A).
+maxSum([1, 2, -4, 5, 1], A).
+maxSum([-1, 2, -3, 3], A).
+
+*/
